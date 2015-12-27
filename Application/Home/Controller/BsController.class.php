bef2646746ae674c81d40525ceabee11e3558aed
AHEADer <dqawveird@gmail.com>
2015-12-27 14:06:30 +0800

    view bug fixed

--- Application/Home/Controller/BsController.class.php
+++ Application/Home/Controller/BsController.class.php
@@ -18,7 +18,7 @@ class BsController extends Controller {
 			$push_data = $model_kt->query("select bs_kt.*,user_teacher.name as
 		tname,user_teacher.officephone,user_teacher.email
 		from bs_kt left join user_teacher on bs_kt.teacher=user_teacher.user where bs_kt.id=".$m[0][$i]);
-			//dump($push_data[0]);
+			dump($push_data[0]);
 			array_push($rs_kt,$push_data[0]);
 			//dump($push_data);
 			//print_r($m);
